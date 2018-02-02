e98a2b8b1f539e3d44508e96b598699e8976735e
alperyilmazny <stfal1@gmail.com>
2018-02-02 15:35:39 -0500

    Added gogle map implemetation

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -57,16 +57,9 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
 
         // Bind other plugins
-        navigator.geolocation.getCurrentPosition(this.onGeoSuccess, this.onGeoError);
+        //navigator.geolocation.getCurrentPosition(this.onGeoSuccess, this.onGeoError);
     }
 };
